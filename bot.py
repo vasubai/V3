fa8d17b11262e360cba6f2846d2decde7d05a28e
Vasubai <90282707+Vasubai@users.noreply.github.com>
2021-11-07 22:18:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ class Bot(Client):
             session_name=SESSION,
             api_id=API_ID,
             api_hash=API_HASH,
-            bot_token=BOT_TOKEN,
+            bot_tok-en=BOT_TOKEN, #worng bot_token
             workers=50,
             plugins={"root": "plugins"},
             sleep_threshold=5,
